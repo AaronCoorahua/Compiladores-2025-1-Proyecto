1efb9e8513ba85d734a79c438f54369524f41a8e
GadielVelarde <jesus.velarde@utec.edu.pe>
2025-06-23 15:55:52 -0500

    actualizandooo todo con el kevin

---
+++ cmake-build-debug/CMakeFiles/proyec.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/gadie/OneDrive/Documentos/compilabs/proyec/exp.cpp" "CMakeFiles/proyec.dir/exp.cpp.obj" "gcc" "CMakeFiles/proyec.dir/exp.cpp.obj.d"
+  "C:/Users/gadie/OneDrive/Documentos/compilabs/proyec/main.cpp" "CMakeFiles/proyec.dir/main.cpp.obj" "gcc" "CMakeFiles/proyec.dir/main.cpp.obj.d"
+  "C:/Users/gadie/OneDrive/Documentos/compilabs/proyec/parser.cpp" "CMakeFiles/proyec.dir/parser.cpp.obj" "gcc" "CMakeFiles/proyec.dir/parser.cpp.obj.d"
+  "C:/Users/gadie/OneDrive/Documentos/compilabs/proyec/scanner.cpp" "CMakeFiles/proyec.dir/scanner.cpp.obj" "gcc" "CMakeFiles/proyec.dir/scanner.cpp.obj.d"
+  "C:/Users/gadie/OneDrive/Documentos/compilabs/proyec/token.cpp" "CMakeFiles/proyec.dir/token.cpp.obj" "gcc" "CMakeFiles/proyec.dir/token.cpp.obj.d"
+  "C:/Users/gadie/OneDrive/Documentos/compilabs/proyec/visitor.cpp" "CMakeFiles/proyec.dir/visitor.cpp.obj" "gcc" "CMakeFiles/proyec.dir/visitor.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/proyec.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyec.dir/exp.cpp.obj"
+  "CMakeFiles/proyec.dir/exp.cpp.obj.d"
+  "CMakeFiles/proyec.dir/main.cpp.obj"
+  "CMakeFiles/proyec.dir/main.cpp.obj.d"
+  "CMakeFiles/proyec.dir/parser.cpp.obj"
+  "CMakeFiles/proyec.dir/parser.cpp.obj.d"
+  "CMakeFiles/proyec.dir/scanner.cpp.obj"
+  "CMakeFiles/proyec.dir/scanner.cpp.obj.d"
+  "CMakeFiles/proyec.dir/token.cpp.obj"
+  "CMakeFiles/proyec.dir/token.cpp.obj.d"
+  "CMakeFiles/proyec.dir/visitor.cpp.obj"
+  "CMakeFiles/proyec.dir/visitor.cpp.obj.d"
+  "libproyec.dll.a"
+  "proyec.exe"
+  "proyec.exe.manifest"
+  "proyec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
