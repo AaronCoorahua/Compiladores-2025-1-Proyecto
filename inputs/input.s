4346585e939fbf58edaabe89ea79f5ea6bf2e8bb
GadielVelarde <jesus.velarde@utec.edu.pe>
2025-06-30 21:46:26 -0500

    implentacionfwi

--- inputs/input.s
+++ inputs/input.s
@@ -1,40 +1,40 @@
 .data
 print_int_fmt: .string "%ld\n"
 print_float_fmt: .string "%f\n"
-x: .quad 0
-y: .quad 0
+i: .quad 0
+result: .quad 0
 .text
 .globl main
+.text
 main:
   pushq %rbp
   movq %rsp, %rbp
-movq $50, %rax
-movq %rax, x(%rip)
-movq $10, %rax
-movq %rax, y(%rip)
-movq  x(%rip), %rax
+movq $1, %rax
+movq %rax, result(%rip)
+movq $2, %rax
+cvtsi2sd %rax, %xmm0
+  movq %rax, i(%rip)
+for_loop_0:
+  movq i(%rip), %rax
+movq $4, %rax
+cvtsi2sd %rax, %xmm0
+  cmpq %rax, i(%rip)
+  jg for_end_0
+movq  result(%rip), %rax
 pushq %rax
-movq  y(%rip), %rax
+movq  i(%rip), %rax
 movq %rax, %rbx
 popq %rax
-cmpq %rbx, %rax
-setg %al
-movzbq %al, %rax
-  cmpq $0, %rax
-  je else_0
-movq  x(%rip), %rax
+imulq %rbx, %rax
+movq %rax, result(%rip)
+  addq $1, i(%rip)
+  jmp for_loop_0
+for_end_0:
+movq  result(%rip), %rax
 movq %rax, %rsi
 leaq print_int_fmt(%rip), %rdi
 movb $1, %al
 call printf@PLT
-  jmp endif_0
-else_0:
-movq  y(%rip), %rax
-movq %rax, %rsi
-leaq print_int_fmt(%rip), %rdi
-movb $1, %al
-call printf@PLT
-endif_0:
   movq $0, %rax
   popq %rbp
   ret
